abcdee4cb6e9171a1b22e39ceac82bea99dc8ad1
xiaokai <xiaokai@kk.com>
2024-04-16 15:41:26 +0700

    更新

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from read_config import get_config
 mail = get_config('mail')
 mail_api = mail.get("api")
 
-invites = get_config("invites")
+pikpak_user = get_config("pikpak_user")
 
 requests = get_config("requests")
 requests_timeout = requests.get("out_time")
--- main.py
+++ main.py
@@ -3,25 +3,65 @@ from chmod import open_url2token
 from ips import thread_get_all_ip
 from mail import create_one_mail, get_new_mail_code
 import config
-
+import asyncio
+from pikpakapi import PikPakApi, PikpakException
 import threading
 
-invites = config.invites
+invites = config.pikpak_user
+
+def_dp = "098poi"
+
+
+def start_share(invite):
+    pikpak_api = PikPakApi(invite.get("mail"), def_dp)
+    # 创建一个事件循环thread_loop
+    main_loop = asyncio.get_event_loop()
+    get_future = asyncio.ensure_future(pikpak_api.login())
+    main_loop.run_until_complete(get_future)
+    fils_id = []
+    for file in invite.get("share", []):
+        get_future = asyncio.ensure_future(pikpak_api.path_to_id(file))
+        main_loop.run_until_complete(get_future)
+        result = get_future.result()
+        fils_id.append(result[-1].get("id"))
+    get_future = asyncio.ensure_future(pikpak_api.file_batch_share(fils_id, expiration_days=7))
+    main_loop.run_until_complete(get_future)
+    result = get_future.result()
+    print(result)
+    return result.get("share_url")
+
+
+def saveUrlToPikpak(mail, url):
+    pikpak_api = PikPakApi(mail, def_dp)
+    main_loop = asyncio.get_event_loop()
+    get_future = asyncio.ensure_future(pikpak_api.login())
+    main_loop.run_until_complete(get_future)
+
+    get_future = asyncio.ensure_future(pikpak_api.offline_download(url))
+    main_loop.run_until_complete(get_future)
+    result = get_future.result()
+    print(result)
 
 
 def runInvite(invite, ips):
     try:
         _mail = create_one_mail()
-        pik_go = PikPak(_mail, "098poi",
+        pik_go = PikPak(_mail, def_dp,
                         captcha_token_callback=open_url2token,
                         main_callback=get_new_mail_code,
-                        invite=str(invite)
+                        invite=str(invite.get("invite_number"))
                         )
         ip, proxy_type = ips.pop(0)
         pik_go.set_proxy(ip, proxy_type)
         pik_go.run_req_2invite()
         if pik_go.isInvise:
             print(f"{invite} 邀请注册成功")
+            url = start_share(invite)
+            saveUrlToPikpak(_mail, url)
+            input_str = f"_mail:{_mail}\n"
+            temp_file = "pikpak_user.txt"
+            with open(temp_file, 'a') as f:  # 设置文件对象
+                f.write(input_str)  # 将字符串写入文件中
             return
         else:
             print(f"{invite} 注册失败！重新注册")
@@ -36,9 +76,10 @@ def runInvite(invite, ips):
 
 
 if __name__ == "__main__":
-    ips = thread_get_all_ip()
+    # ips = thread_get_all_ip()
     for invite in invites:
-        runInvite(invite.get("invite_number"), ips)
+        # runInvite(invite, ips)
+        start_share(invite)
     # ths = []
     # for invite in invites:
     #     th = threading.Thread(target=runInvite, args=(invite.get("invite_number"), ips))
