93a6823cae6fae2a842883aa6e90efe88e47f4fc
xiaokai <xiaokai@kk.com>
2024-08-29 19:52:21 +0700

    server 增加内容

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import time
 from flask import Flask, request, jsonify
 from captcha import slider_validation
 app = Flask(__name__)
@@ -7,7 +8,13 @@ app = Flask(__name__)
 def get_data():
     # 获取请求中的 JSON 数据
     url = request.args.get("url")
-    token = slider_validation(url)
+    for count in range(3):
+        try:
+            token = slider_validation(url)
+            break
+        except:
+            token = None
+            time.sleep(5)
     # 这里可以添加处理 URL 的逻辑
     # 示例：假设我们只是返回一些静态数据
     if token and token != "":
@@ -16,13 +23,14 @@ def get_data():
             'token': token,
             "code": 200,
         }
+        return jsonify(response_data)
     else:
         response_data = {
             'url_received': url,
             "code": -1,
         }
-    return jsonify(response_data)
+        return jsonify(response_data), 400
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5245)
+    app.run(debug=True, port=5243)
